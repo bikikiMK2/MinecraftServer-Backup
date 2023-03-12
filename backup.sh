65c97b98c55de6928740807c03fc0d813da63d64
bikikiMK2 <bikiki.bikiki.9180@gmail.com>
2023-03-13 04:08:45 +0900

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 time=60
-screen -S minecraft -X stuff 'say §d自動バックアップを開始します' "$(echo -ne '\r')"
-screen -S minecraft -X stuff 'say §4'$time'§秒後にサーバーを停止後再起動を実施' "$(echo -ne '\r')"
+screen -S minecraft -X stuff 'say §d 自動バックアップを開始します' "$(echo -ne '\r')"
+screen -S minecraft -X stuff 'say §4'$time'§秒後にサーバーを再起動します' "$(echo -ne '\r')"
+sleep "$(exrp time - 10)"
+screen -S minecraft -X stuff 'say §d まもなく再起動が開始されます' "$(echo -ne '\r')"
+sleep 10
+screen -S minecraft -X stuff 'stop' "$(echo -ne '\r')"
+abouttime=10
+sleep $abouttime
+screen -S minecraft -X stuff 'zip world.zip worlds/world/' "$(echo -ne '\r')"
+screen -S minecraft -X stuff 'mv world.zip backup/' "$(echo -ne '\r')"
+mv /data/minecraft/bedrock/1.19.63-multi/worlds/world/world.zip world'{'"$(data'+%Y%m%d%k%H')"'}'  "$(echo -ne '\r')"
+screen -S minecraft -X stuff 'rm -r worlds/backup/word.zip' "$(echo -ne '\r')"
+screen -S minecraft -X stuff 'LD_LIBRARY_PATH=. ./bedrock_server' "$(echo -ne '\r')"
+                              
+
+
+
+
+
+
+
 
