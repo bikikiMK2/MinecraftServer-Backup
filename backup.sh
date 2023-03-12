651670494c2395fc56e5dac96aabd7c9857c52b5
bikikiMK2 <bikiki.bikiki.9180@gmail.com>
2023-03-13 02:10:23 +0900

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+time=60
+screen -S minecraft -X stuff 'say §d自動バックアップを開始します' `echo -ne '\r'`
+screen -S minecraft -X stuff 'say §4'$time'§秒後にサーバーを停止後再起動を実施' `echo -ne '\r'`
+time2=`expr $time - 10`
+sleep `expr $time2`
+
