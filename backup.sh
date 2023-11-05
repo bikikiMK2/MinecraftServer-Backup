29c37ad0a845e588973d8c40962c447a4961e5f5
bikikiMK2 <bikiki.bikiki.9180@gmail.com>
2023-11-05 12:35:39 +0900

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
-time=60
-screen -S minecraft -X stuff 'say §d 自動バックアップを開始します'"$(echo -ne '\r')"
-screen -S minecraft -X stuff "say §4$time§d 秒後にサーバーを再起動します""$(echo -ne '\r')"
-sleep "$(exrp time - 10)"
-screen -S minecraft -X stuff 'say §d まもなく再起動が開始されます'"$(echo -ne '\r')"
-sleep 10
-screen -S minecraft -X stuff 'stop'"$(echo -ne '\r')"
-abouttime=10
-sleep $abouttime
-cp -r world/ backups/
-# shellcheck disable=SC2164
-cd backups/
-zip -r world"{""$(date +%Y.%m.%d.%H%M)""}".zip world/
-rm -r world/
-screen -S minecraft -X stuff 'LD_LIBRARY_PATH=. ./bedrock_server'"$(echo -ne '\r')"
+
+screen -S <name> -X stuff 'say §d自動バックアップを開始します'"$(echo -ne '\r')"
+
+dirpass = #GitHubのあれを参照するようにしたい
+
+zip -r dirpass + world"{""$(date+%Y.%m.%d.%H%M)""}".zip "dirpass" + world/
